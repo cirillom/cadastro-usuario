89a761026593dd9c9268d20bdefeacab88991da1
Matheus Cirillo <matheuscirillo@usp.br>
2024-05-20 23:59:47 -0300

    ready for full testing

--- seleniumTest.py
+++ seleniumTest.py
@@ -71,24 +71,44 @@ def make_test(tests, testCount, name, x_name_result, year, x_year_result, email,
 nameTests = [
     ("Matheus", ""), #Caso com apenas letras
     ("m4theus", "inválido"), #Caso com letras e número
+    ("matheus cirillo", "inválido"), #Caso com letras e número
+    ("m@theus", "inválido"), #Caso com letras e caracteres
+    ("mat", "inválido") #Caso com menos de 6 letras
 ]
 
 # input (year, expected year result)
 yearTests = [
     ("2003", ""), #caso válido
+    ("1900", ""), #caso de borda válido
+    ("2022", ""), #caso de borda válido
     ("2023", "inválido"), #caso onde é maior que 2022
+    ("1899", "inválido"), #caso onde é menor que 1900
+    ("matheus", "inválido") #caso com letras
 ]
 
 # input (email, expected email result)
 emailTests = [
+    ("usuario@dominio.net", ""),
+    ("usu4rio@dominio.com", ""),
+    ("matheus@usp.br", ""),
     ("123456789@dominio.org", ""),
-    ("usu@rio@dominio.com", "inválido"),  # letras, números e símbolos antes do @
+    ("usu!rio@dominio.com", "inválido"),  # letras, números e símbolos antes do @
+    ("@dominio.com", "inválido"),  # nada antes do @
+    ("usuario@dom!nio.com", "inválido"),  # letras, números e símbolos entre @ e .
+    ("usuario@.com", "inválido"),  # nada entre @ e .
+    ("usuario@dominio.usp", "inválido")  # final usp depois do .
 ]
 
 # input (password, expected password result, expected password strength)
 passwordTests = [
+    ("a1!b", "inválida", "0"), #senha com menos de 6 caracteres
+    ("a1!b2c3d4@@f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0", "inválida", "0"), #senha com mais de 20 caracteres
+    ("abcdefgh123", "inválida", "0"), #senha sem caracter especial
+    ("abcdefgh!", "inválida", "0"), #senha sem número
     ("12345678!", "inválida", "0"), #senha sem letra
     ("abcde1!", "fraca", "10"), #senha fraca
+    ("Abcde1!fgh", "moderada", "20"), #senha moderada
+    ("ABcd123!@fgHI", "forte", "30") #senha forte
 ]
 
 tests = {}
@@ -101,12 +121,12 @@ f.write("Test,Name,ExpectedNameResult,Year,ExpectedYearResult,Email,ExpectedEmai
 
 all_tests = itertools.product(nameTests, yearTests, emailTests, passwordTests)
 
+#hardcoded tests to check password handling name and year
 year_in_pass = (("Matheus", ""), ("2003", ""), ("123456789@dominio.org", ""), ("pabx!@#2003", "inválida", "0"))
 name_in_pass = (("Matheus", ""), ("2003", ""), ("123456789@dominio.org", ""), ("matheus123@", "inválida", "0"))
 all_tests = list(all_tests)
 all_tests.append(year_in_pass)
 all_tests.append(name_in_pass)
-
 
 # Use itertools.product to create a Cartesian product of the tests
 for (name, x_name_result), (year, x_year_result), (email, x_email_result), (password, x_password_result, x_strength_result) in tqdm(itertools.product(nameTests, yearTests, emailTests, passwordTests), total=total_iterations):
@@ -121,16 +141,22 @@ for key, value in tests.items():
     if value["result"] == True:
         successfullTests += 1
 
+f = open("results.csv", "w")
+
 print(colored(f"Total of {successfullTests} / {testCount}", "blue"))
+f.write(f"Total of {successfullTests} / {testCount}")
 for key, value in tests.items():
     if value["result"] == True:
         print(colored(f"Test {key}: Successful", "green"))
+        f.write(f"Test {key}: Successful\n")
     else:
         print(colored(f"Test {key}: Failed on: ", "red"))
+        f.write(f"Test {key}: Failed on: ")
         for k, v in value.items():
             if k != "result":
                 if v[2] == False:
                     print(colored(f"\t{k}: got \"{v[0]}\" ", "red"))
+                    f.write(f"\t{k}: got \"{v[0]}\"\n")
 
 # Close the browser
 driver.quit()
